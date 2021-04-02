4c2d7a889e35df02f14b5264a459cb8f33d15f51
ramseshts <ramseshutasoit77@gmail.com>
2021-04-03 05:24:23 +0700

    website using gatsby with graphql

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const About = () => {
+    return (
+        <Layout>
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        </Layout>
+    );
+}
+
+export default About
